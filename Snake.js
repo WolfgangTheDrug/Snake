4a7bebcf2e53d18c62f67b38a17f88b8ba3b4ddc
Krzysztof Kamiński <bardak_widz@wp.pl>
2020-03-28 21:07:47 +0100

    Basic setup for the game

---
+++ Snake.js
@@ -0,0 +1,94 @@
+/* ******************* *
+ *   Pallette: START   *
+ * ******************* */
+
+const yellowGreen = {
+  dark:
+    [
+      '#146C23',
+      '#1E851A',
+      '#409E21'
+    ],
+  neutral:
+    [
+      '#6AB629',
+      '#9ACD32',
+      '#D4DA58'
+    ],
+  light:
+    [
+      '#E6CE7F',
+      '#F0CBA8',
+      '#F8DBD3'
+    ]
+};
+
+const skyBlue = {
+  dark:
+    [
+      '#14306C',
+      '#1A5385',
+      '#217E9E'
+    ],
+  neutral:
+    [
+      '#29B0B6',
+      '#32CDB3',
+      '#58DAA0'
+    ],
+  light:
+    [
+      '#7FE69B',
+      '#A9F0A8',
+      '#DEF8D3'
+    ]
+};
+
+const slateBlue = {
+  dark:
+    [
+      '#6C145C',
+      '#821A85',
+      '#7F219E'
+    ],
+  neutral:
+    [
+      '#7529B6',
+      '#6532CD',
+      '#5E58DA'
+    ],
+  light:
+    [
+      '#7F97E6',
+      '#A8CDF0',
+      '#D3F0F8'
+    ]
+};
+
+const crimson = {
+  dark:
+    [
+      '#6C4F14',
+      '#854C1A',
+      '#9E4121'
+    ],
+  neutral:
+    [
+      '#B62F29',
+      '#CD324D',
+      '#DA5893'
+    ],
+  light:
+    [
+      '#E67FCA',
+      '#EFA8F0',
+      '#EDD3F8'
+    ]
+};
+
+const pallette = {
+  yellowGreen: yellowGreen,
+  skyBlue: skyBlue,
+  slateBlue: slateBlue,
+  crimson: crimson
+};
